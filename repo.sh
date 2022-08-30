e57a065b2b8e2bd8cae8fc033bee4ca3b510958f
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2022-08-31 00:20:43 +0530

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,2 +1,5 @@
 x="$(curl  -X POST  -H "Accept: application/vnd.github+json"  -H "Authorization: token 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" https://api.github.com/orgs/kishore-sharma-sundaram/repos -d '{"name":"gokul-pls","description":"This is for  repository","homepage":"https://github.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}')"
 echo "$x"
+
+
+curl -s https://api.github.com/orgs/kishore-sharma-sundaram/gokul-pls | grep 'created_at'|cut -d: -f2-
